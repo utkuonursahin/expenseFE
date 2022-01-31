88d07298f5c813eac5a483f606491e70ba9a1313
melihyelman <myelman17@gmail.com>
2022-01-31 10:30:42 +0300

    add routes and protected route

---
+++ src/pages/ProtectedRoute.js
@@ -0,0 +1,11 @@
+import { Outlet, Navigate, useLocation } from 'react-router-dom';
+import { useAuth } from '../context/AuthContext';
+
+function ProtectedRoute({ element, ...rest }) {
+    const { loggedIn } = useAuth();
+    const location = useLocation();
+
+    return loggedIn ? <Outlet /> : <Navigate to="/" state={{ from: location }} replace />;
+}
+
+export default ProtectedRoute;
